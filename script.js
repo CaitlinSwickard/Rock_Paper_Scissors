ef099786cd663988d540a8db5d6197df7f9b9fe2
Caitlin Swickard <caitlin6029@gmail.com>
2021-12-30 09:50:49 -0700

    set up conditional checks

--- script.js
+++ script.js
@@ -14,4 +14,32 @@ function playGame() {
   if (!userChoice) {
     return;
   }
- }
+
+  // edge case for if user does not put in capital letters to make computer comparison easier
+  userChoice = userChoice.toUpperCase();
+
+  // creates random selection for computer to select
+  let index = Math.floor(Math.random() * options.length);
+  let computerChoice = options[index];
+
+  window.alert("The computer chose " + computerChoice);
+
+  //  if selection is the same it is a tie
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("Its a tie!!")
+    // checks win conditions for the player
+  } else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You win!!")
+    // if other conditions fail player losses
+  } else {
+    losses++;
+    window.alert("You lose!!")
+  }
+
+}
